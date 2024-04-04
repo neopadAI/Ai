df8bf1bb82f528aa80954ca741bd613f568ef897
Neopad__ <164239736+neopadAI@users.noreply.github.com>
2024-04-04 15:01:05 +0200

    Update __init__.py
    Signed-off-by: Neopad__ <164239736+neopadAI@users.noreply.github.com>

--- monkey_1__/__init__.py
+++ monkey_1__/__init__.py
@@ -1,1 +1,16 @@
-print('monkeyAI')
+possibility = 0
+initial = ''
+monkey = [
+  'hello',
+  'test',
+  'testo'
+]
+def request(text):
+  for monkeyA in monkey:
+    if monkeyA in text:
+      print('monkey: ' + initial + monkeyA)
+      max_possibility =+ 1
+      if max_possibility == possibility:
+        pass
+      else:
+        break
