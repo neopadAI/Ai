aaaadbb27b52e0f971a501104f8de71b347edbaf
Neopad__ <164239736+neopadAI@users.noreply.github.com>
2024-04-04 14:44:22 +0200

    Update main.py
    Signed-off-by: Neopad__ <164239736+neopadAI@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 w = ''
 def model(model_name):
-  if model_name == 'nnc-- nepsi 1.0 - f':
-    import nepsi_1__.__init__ as n
+  if model_name == 'nnc-- monkey 1.0 - f':
+    import monkey_1__.__init__ as n
     if w == 'infinite':
       while True:
         n.request(input('Enter your question: '))
