697532a217b0de751ed46c3a07553f1ec3275a13
Neopad__ <164239736+neopadAI@users.noreply.github.com>
2024-04-04 18:14:09 +0200

    Create test.py
    Signed-off-by: Neopad__ <164239736+neopadAI@users.noreply.github.com>

---
+++ test.py
@@ -0,0 +1,4 @@
+import main as monkey # import file main asi monkey
+monkey.model = 'nnc-- monkey 1.0 - f' # select a model
+monkey.request = 'hello' # insert a question
+monkey.__request__() # start the file
